ddaffb7ca767eb760aa15e00c8d0a6a5f4222bd7
Kalshiev <haescobedo@gmail.com>
2026-03-25 20:24:42 -0600

    Add initial project files including go.mod, main.go, index.html, and output binary

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to start server")
+	}
+}
