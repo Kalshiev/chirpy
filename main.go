8804bf6a927428161acc93a31be17c49d79f86bf
Kalshiev <haescobedo@gmail.com>
2026-03-28 08:54:34 -0600

    Implement metrics middleware and health check endpoints

--- main.go
+++ main.go
@@ -2,25 +2,64 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Count Incremented!")
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(""))))
+	count := apiConfig{}
+	mux.Handle("/app/", http.StripPrefix("/app/", count.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.Handle("/assets", http.FileServer(http.Dir("/assets/logo.png")))
 
-	mux.HandleFunc("/healthz", handler)
+	mux.HandleFunc("/healthz", handlerReadiness)
 
-	server := http.Server{
+	mux.HandleFunc("/metrics", count.handlerCount)
+	mux.HandleFunc("/reset", count.handlerReset)
+
+	srv := &http.Server{
 		Handler: mux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("failed to start server")
-		fmt.Println(err)
-	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+
+}
+
+func (cfg *apiConfig) handlerCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits.Swap(0)
+	log.Printf("Count Reset")
 }
